1ed90cdf338a29e4b57a4dc2e51441c5264a4ade
Chris Weyl <cweyl@alumni.drew.edu>
2015-04-13 11:53:28 -0700

    initial commit

---
+++ lib/Acme/Author/RSRCHBOY/TravisTinkering.pm
@@ -0,0 +1,26 @@
+package Acme::Author::RSRCHBOY::TravisTinkering;
+
+# ABSTRACT: The great new Acme::Author::RSRCHBOY::TravisTinkering!
+
+use Moose;
+use namespace::autoclean;
+
+use Acme::Bleach;
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+    What are you even thinking, using this.
+
+=head1 DESCRIPTION
+
+This will never, ever be released to the CPAN.  (I think)  It's just a scratch
+project designed to allow me to tinker with Travis CI builds w/o constantly
+trying things out in a "travis/tinkering" branch of a real project.
+
+=head1 SEE ALSO
+
+=cut
